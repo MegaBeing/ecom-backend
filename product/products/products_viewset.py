2f6cb8ac931af244ded7bd065a411436a48cf9b7
MegaBeing <jain.sahil58158@gmail.com>
2024-08-28 11:16:04 +0530

    changes in filter_products to add exclusive and collection

--- product/products/products_viewset.py
+++ product/products/products_viewset.py
@@ -20,13 +20,18 @@ class ProductViewSet(viewsets.ReadOnlyModelViewSet):
         min_value = request.query_params.get('min_value')
         max_value = request.query_params.get('max_value')
         color = request.query_params.get('color')
-
+        collection = request.query_params.get('collection')
+        is_exclusive = request.query_params.get('is_exclusive')
         queryset = self.get_queryset()
 
         if search:
             queryset = queryset.filter(product__name__icontains=search)
         if category:
             queryset = queryset.filter(product__category=category)
+        if collection:
+            queryset = queryset.filter(product__collection=collection)
+        if is_exclusive:
+            queryset = queryset.filter(product__isExclusive=True)
         if min_value:
             queryset = queryset.filter(price__gte=float(min_value))
         if max_value:
